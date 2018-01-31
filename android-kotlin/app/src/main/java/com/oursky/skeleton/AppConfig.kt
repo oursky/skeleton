a9767401e7892810c2fa0260df57deca7f216ff5
hidden-dimensions <hidden-dimensions@users.noreply.github.com>
2018-01-31 12:30:17 +0800

    improved splash screen handling

--- android-kotlin/app/src/main/java/com/oursky/skeleton/AppConfig.kt
+++ android-kotlin/app/src/main/java/com/oursky/skeleton/AppConfig.kt
@@ -2,6 +2,6 @@ package com.oursky.skeleton
 
 @Suppress("unused")
 object AppConfig {
-    const val SPLASH_DURATION = 2000L
+    const val SPLASH_DURATION = 2000L // splash screen duration in ms, 0 to disable
     const val SERVER_BASE = "http://10.0.2.2:8080/rest/"
 }
